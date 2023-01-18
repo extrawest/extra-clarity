2f5af996c6c31992f956804519dbc4ed4e9ccd53
andrii.kapustin-ew <andrii.kapustin@extrawest.com>
2023-01-18 01:44:16 +0100

    fix dialog title

--- projects/extra-clarity/dialog/src/directives/dialog-title.directive.ts
+++ projects/extra-clarity/dialog/src/directives/dialog-title.directive.ts
@@ -3,5 +3,8 @@ import { Directive } from '@angular/core';
 @Directive({
   selector: '[dialog-title]',
   exportAs: 'dialogTitle',
+  host: {
+    class: 'modal-title',
+  },
 })
 export class DialogTitleDirective {}
