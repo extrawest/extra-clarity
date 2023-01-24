c94d08b759671e89a574ebeec76babd7fc026e85
andrii.kapustin-ew <andrii.kapustin@extrawest.com>
2023-01-24 18:31:00 +0100

    fix dialog module

--- projects/extra-clarity/dialog/src/containers/dialog-container/dialog.container.ts
+++ projects/extra-clarity/dialog/src/containers/dialog-container/dialog.container.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectionStrategy, Component, Inject, Input } from '@angular/core';
+import { Location } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Inject, Input, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, SubscriptionLike } from 'rxjs';
 
 import { DialogConfig } from '../../dialog-config';
 import { DialogRef } from '../../dialog-ref';
@@ -10,15 +12,26 @@ import { DialogSize } from '../../types/dialog-types';
   templateUrl: 'dialog.container.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DialogContainer {
+export class DialogContainer implements OnInit, OnDestroy {
   @Input() public size: DialogSize = 'md';
   @Input() public closable = true;
   @Input() public closableBackdrop = true;
 
+  private locationSubscription: SubscriptionLike = Subscription.EMPTY;
+
   constructor(
     private readonly dialogRef: DialogRef,
+    private readonly location: Location,
     @Inject(DIALOG_CONFIG) public readonly config: DialogConfig,
   ) {}
+
+  public ngOnInit(): void {
+    this.locationSubscription = this.location.subscribe(() => this.onClose());
+  }
+
+  public ngOnDestroy(): void {
+    this.locationSubscription.unsubscribe();
+  }
 
   public onClose(): void {
     this.dialogRef.close();
