c10ad97dd5ebaf0ab1ca7cf828ed1f1dc1d28c5a
dmytro-batazov-ew <117995904+dmytro-batazov-ew@users.noreply.github.com>
2023-02-16 22:00:43 +0200

    Remove ClrAlertModule from DialogModule as it is unused now

--- projects/extra-clarity/dialog/src/dialog.module.ts
+++ projects/extra-clarity/dialog/src/dialog.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { ClrAlertModule, ClrLoadingButtonModule, ClrLoadingModule, ClrModalModule } from '@clr/angular';
+import { ClrLoadingButtonModule, ClrLoadingModule, ClrModalModule } from '@clr/angular';
 
 import { ConfirmationDialogComponent, DialogContainer } from './containers';
 import { DialogCloseDirective, DialogContentDirective, DialogFooterDirective, DialogTitleDirective } from './directives';
@@ -21,7 +21,6 @@ const DIRECTIVES = [
   ],
   imports: [
     CommonModule,
-    ClrAlertModule,
     ClrModalModule,
     ClrLoadingModule,
     ClrLoadingButtonModule,
