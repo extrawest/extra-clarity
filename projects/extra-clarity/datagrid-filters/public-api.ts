7d480959e418ad47fc989097b6254825109a368a
andrii.kapustin-ew <andrii.kapustin@extrawest.com>
2023-03-28 23:38:53 +0200

    update datagrid filters component

--- projects/extra-clarity/datagrid-filters/public-api.ts
+++ projects/extra-clarity/datagrid-filters/public-api.ts
@@ -1,2 +1,3 @@
 export * from './src/enumerated-value-filter';
 export * from './src/partial-string-filter';
+export * from './src/interfaces/filter-state.interface';
--- projects/extra-clarity/datagrid-filters/src/interfaces/filter-state.interface.ts
+++ projects/extra-clarity/datagrid-filters/src/interfaces/filter-state.interface.ts
@@ -1,4 +1,4 @@
-export interface FilterState<T> {
+export interface FilterState<T = unknown> {
   property: string | number;
   value: T;
 }
