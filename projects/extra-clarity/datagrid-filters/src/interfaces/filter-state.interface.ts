a185dc838f78f096c6dabfc0692442b7d95b5bab
dmytro-batazov-ew <117995904+dmytro-batazov-ew@users.noreply.github.com>
2023-05-10 11:43:17 +0300

    Remove default generic types from datagrid filter interaces

--- projects/extra-clarity/datagrid-filters/src/interfaces/filter-state.interface.ts
+++ projects/extra-clarity/datagrid-filters/src/interfaces/filter-state.interface.ts
@@ -1,15 +1,15 @@
-export interface FilterState<T = unknown> {
+export interface FilterState<T> {
   property: string | number;
   value: T | undefined;
 }
 
-export interface EnumFilterOption<T = unknown> {
+export interface EnumFilterOption<T> {
   value: T;
   label?: string;
   selectedByDefault?: boolean;
 }
 
-export interface EnumGroupFilterOption<T = unknown> {
+export interface EnumGroupFilterOption<T> {
   label: string;
   items: readonly EnumFilterOption<T>[];
   expandedByDefault?: boolean;
