066362e2c2c75f43610df0711c7ea6495dbc7bf1
dmytro-batazov-ew <117995904+dmytro-batazov-ew@users.noreply.github.com>
2023-05-15 23:36:06 +0300

    Add router-link-wrapper component

---
+++ projects/extra-clarity/router-link-wrapper/src/router-link-wrapper.component.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-router-link-wrapper',
+  template: `
+    <a [routerLink]="fwdRouterLink" *ngIf="enabled; else content">
+      <ng-container *ngTemplateOutlet="content" />
+    </a>
+    <ng-template #content>
+      <ng-content/>
+    </ng-template>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterLink,
+  ],
+})
+export class LinkWrapperComponent {
+  @Input() enabled: unknown = false;
+  @Input() fwdRouterLink: unknown[] | string | null | undefined;
+}
