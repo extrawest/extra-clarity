58b9866261bfc055c07596d54fbc58fb15929899
dmytro-batazov-ew <117995904+dmytro-batazov-ew@users.noreply.github.com>
2023-05-20 16:17:41 +0300

    Move stories folder into .storybook; correct some configs accordingly

--- projects/extra-clarity/.storybook/main.ts
+++ projects/extra-clarity/.storybook/main.ts
@@ -1,20 +1,21 @@
-import type { StorybookConfig } from "@storybook/angular";
+import type { StorybookConfig } from '@storybook/angular';
+
 const config: StorybookConfig = {
   stories: [
-    "../src/stories/**/*.mdx",
-    "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"
+    './stories/**/*.mdx',
+    './stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
   framework: {
-    name: "@storybook/angular",
+    name: '@storybook/angular',
     options: {},
   },
   docs: {
-    autodocs: "tag",
+    autodocs: 'tag',
   },
   core: {
     disableTelemetry: true,
