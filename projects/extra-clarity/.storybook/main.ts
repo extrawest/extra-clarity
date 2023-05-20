af6e1ae8dc5d71d5113d0ae21632939d24088a2b
dmytro-batazov-ew <117995904+dmytro-batazov-ew@users.noreply.github.com>
2023-05-20 16:00:01 +0300

    Fix some configs

--- projects/extra-clarity/.storybook/main.ts
+++ projects/extra-clarity/.storybook/main.ts
@@ -1,6 +1,9 @@
 import type { StorybookConfig } from "@storybook/angular";
 const config: StorybookConfig = {
-  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: [
+    "../src/stories/**/*.mdx",
+    "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
