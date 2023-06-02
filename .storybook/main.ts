1925eab1a1e5afc0ef1b0b153df03424181e8d03
dmytro-batazov-ew <117995904+dmytro-batazov-ew@users.noreply.github.com>
2023-06-02 22:54:24 +0300

    Setup compodoc for auto-generating documentation on storybook building

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -16,6 +16,7 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: 'tag',
+    defaultName: 'Docs',
   },
   core: {
     disableTelemetry: true,
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,9 @@
+import { setCompodocJson } from '@storybook/addon-docs/angular';
 import type { Preview } from '@storybook/angular';
+
+import docJson from '../documentation.json';
+
+setCompodocJson(docJson);
 
 const preview: Preview = {
   parameters: {
