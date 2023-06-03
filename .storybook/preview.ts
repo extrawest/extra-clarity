44895ae7dcff974fbda804609285f0d5f08899e9
dmytro-batazov-ew <117995904+dmytro-batazov-ew@users.noreply.github.com>
2023-06-04 00:18:42 +0300

    Show additional info for inputs and outputs in Controls Panel

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -7,12 +7,12 @@ setCompodocJson(docJson);
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/,
       },
+      expanded: true,
     },
   },
 };
