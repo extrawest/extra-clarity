cb67ded19ed8a63666c0bc1f03f1eae8bd07e534
dmytro-batazov-ew <117995904+dmytro-batazov-ew@users.noreply.github.com>
2023-06-05 00:19:29 +0300

    Fix lint error

--- .storybook/stories/numeric-field/helpers/numeric-field-demo.component.ts
+++ .storybook/stories/numeric-field/helpers/numeric-field-demo.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ClrInputModule } from '@clr/angular';
 
