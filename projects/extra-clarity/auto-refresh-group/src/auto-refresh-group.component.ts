6690cc131cd4224dd62d092dbf9668e303c6d44f
dmytro-batazov-ew <117995904+dmytro-batazov-ew@users.noreply.github.com>
2023-06-04 14:38:07 +0300

    Adapt code-style for compodoc docs generation

--- projects/extra-clarity/auto-refresh-group/src/auto-refresh-group.component.ts
+++ projects/extra-clarity/auto-refresh-group/src/auto-refresh-group.component.ts
@@ -1,18 +1,12 @@
 import { CommonModule } from '@angular/common';
 import {
-  ChangeDetectionStrategy,
-  Component,
-  EventEmitter,
-  Input,
-  OnChanges,
-  Output,
-  SimpleChanges,
+  ChangeDetectionStrategy, Component, EventEmitter, Input, OnChanges, Output, SimpleChanges,
 } from '@angular/core';
 import { CdsIconModule } from '@cds/angular';
 import { ClarityIcons, errorStandardIcon, refreshIcon } from '@cds/core/icon';
 import { AutoRefreshComponent } from '@extrawest/extra-clarity/auto-refresh';
 
-ClarityIcons.addIcons(refreshIcon, errorStandardIcon);
+export const DEFAULT_PERIOD_SEC = 60;
 
 @Component({
   selector: 'ec-auto-refresh-group',
@@ -27,29 +21,50 @@ ClarityIcons.addIcons(refreshIcon, errorStandardIcon);
   ],
 })
 export class AutoRefreshGroupComponent implements OnChanges {
-  @Input() public failed = false;
-  @Input() public refreshing = false;
-  @Input() public autoRefreshBlocked = false;
-  @Input() public autoRefreshEnabled = true;
-  @Input() public period = 60;
-  @Input() public useAutoRefresh = true;
+  @Input()
+  public failed = false;
 
-  @Output() public readonly refresh = new EventEmitter<void>();
-  @Output() public readonly autoRefreshToggled = new EventEmitter<boolean>();
+  @Input()
+  public refreshing = false;
 
-  public lastFetchTimestamp = Date.now();
+  @Input()
+  public autoRefreshBlocked = false;
 
-  public ngOnChanges(changes: SimpleChanges): void {
+  @Input()
+  public autoRefreshEnabled = true;
+
+  /** Refreshing period in seconds */
+  @Input()
+  public period = DEFAULT_PERIOD_SEC;
+
+  @Input()
+  public useAutoRefresh = true;
+
+  /** `EventEmitter<void>` */
+  @Output()
+  public readonly refresh = new EventEmitter<void>();
+
+  /** `EventEmitter<boolean>` */
+  @Output()
+  public readonly autoRefreshToggled = new EventEmitter<boolean>();
+
+  protected lastFetchTimestamp = Date.now();
+
+  constructor() {
+    ClarityIcons.addIcons(refreshIcon, errorStandardIcon);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
     if (('refreshing' in changes) && !changes['refreshing'].currentValue) {
       this.lastFetchTimestamp = Date.now();
     }
   }
 
-  public onAutoRefreshChanged(state: boolean): void {
+  protected onAutoRefreshChanged(state: boolean): void {
     this.autoRefreshToggled.emit(state);
   }
 
-  public onRefresh(): void {
+  protected onRefresh(): void {
     this.refresh.emit();
   }
 }
