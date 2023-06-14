bc2854d05b7bff68f4c83a069f91428cc37e0471
dmytro-batazov-ew <117995904+dmytro-batazov-ew@users.noreply.github.com>
2023-06-14 23:22:48 +0300

    Extract common styles for docs into external constant

---
+++ .storybook/helpers/styles.ts
@@ -0,0 +1,22 @@
+export const docsStylePatch = `
+  h3 {
+    margin: 36px 0 12px !important;
+    font-size: 20px !important;
+  }
+
+  p {
+    margin-top: 12px !important;
+  }
+
+  .docblock-argstable-body p {
+    margin: 0 !important;
+  }
+
+  .docblock-argstable {
+    margin-top: 0 !important;
+  }
+
+  .docblock-source {
+    margin: 12px 0 18px !important;
+  }
+`;
