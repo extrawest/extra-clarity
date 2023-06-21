a7bc18652034132ca4b7493fd18d63e1880d7363
dmytro-batazov-ew <117995904+dmytro-batazov-ew@users.noreply.github.com>
2023-06-22 00:26:21 +0300

    Rework datagrid-cell-wrapper component; add annotations for compodoc

--- projects/extra-clarity/datagrid-cell-wrapper/src/datagrid-cell-wrapper.component.ts
+++ projects/extra-clarity/datagrid-cell-wrapper/src/datagrid-cell-wrapper.component.ts
@@ -14,11 +14,51 @@ import { ButtonCopyToClipboardComponent } from '@extrawest/extra-clarity/button-
   ],
 })
 export class DatagridCellWrapperComponent {
-  @Input() truncate = false;
-  @Input() withCopyBtn = false;
-  @Input() btnInline = false;
-  @Input() textToCopy?: string | undefined;
+  /**
+   * Where to place the copy-to-clipboard button
+   * when the content length is less than the cell width:
+   * * `true` = next to the content
+   * * `false` = in the top right corner of the cell
+   *
+   * Applied only if `[withCopyBtn]="true"`
+   */
+  @Input()
+  public btnInline = false;
 
-  @Output() copied = new EventEmitter<string>();
-  @Output() failed = new EventEmitter<unknown>();
+  /**
+   * A string to copy into the clipboard on pressing the copy-to-clipboard button.
+   *
+   * Usually it is a stringified value of the cell.
+   *
+   * Applied only if `[withCopyBtn]="true"`
+   */
+  @Input()
+  public textToCopy?: string | null;
+
+  /**
+   * How to handle overflow of the cell content:
+   * * `true` = truncate with ellipsis
+   * * `false` = break into a new line
+   */
+  @Input()
+  public truncate = false;
+
+  /** Show a copy-to-clipboard button in the wrapper */
+  @Input()
+  public withCopyBtn = false;
+
+  /**
+   * Emits a `string` successfully copied to the clipboard
+   * by pressing the copy-to-clipboard button
+   */
+  @Output()
+  public copied = new EventEmitter<string>();
+
+  /**
+   * Emits an `unknown` value if copying to the clipboard failed.
+   *
+   * Caught from `navigator.clipboard.writeText()`.
+   */
+  @Output()
+  public failed = new EventEmitter<unknown>();
 }
