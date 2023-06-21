12ac66567282819a035cf7c3786d0b17ac50ffe7
dmytro-batazov-ew <117995904+dmytro-batazov-ew@users.noreply.github.com>
2023-06-21 17:54:39 +0300

    Rework button-copy-to-clipboard component; add annotations for compodoc

--- projects/extra-clarity/button-copy-to-clipboard/src/button-copy-to-clipboard.component.ts
+++ projects/extra-clarity/button-copy-to-clipboard/src/button-copy-to-clipboard.component.ts
@@ -1,6 +1,12 @@
 import { CommonModule } from '@angular/common';
 import {
-  ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, HostBinding, Input, Output,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  EventEmitter,
+  inject,
+  Input,
+  Output,
 } from '@angular/core';
 import { CdsIconModule } from '@cds/angular';
 import { ClarityIcons, copyIcon } from '@cds/core/icon';
@@ -8,7 +14,11 @@ import { ClrLoadingState } from '@clr/angular';
 
 import { animations } from './button-copy-to-clipboard.animations';
 
-ClarityIcons.addIcons(copyIcon);
+export const BUTTON_DEFAULTS = {
+  buttonSizePx: 24,
+  iconSizePx: 16,
+  title: 'Copy to Clipboard',
+} as const;
 
 @Component({
   selector: 'ec-button-copy-to-clipboard',
@@ -23,30 +33,98 @@ ClarityIcons.addIcons(copyIcon);
   animations,
 })
 export class ButtonCopyToClipboardComponent {
-  @HostBinding('@.disabled') disableAnimation = false;
+  /** Whether the button is disabled (blocked) */
+  @Input()
+  public disabled = false;
 
-  @Input() set noAnimation(disable: boolean) {
-    this.disableAnimation = disable;
+  /**
+   * Height in pixels for the button. Includes the border size if `[withBorder]="true"`.
+   *
+   * If not provided, then a value from the `sizePx` input is used instead.
+   * */
+  @Input()
+  public heightPx?: number;
+
+  /** Size (width & height) in pixels for icons and spinners inside of the button */
+  @Input()
+  public iconSizePx: number = BUTTON_DEFAULTS.iconSizePx;
+
+  /** An optional text label for the default state */
+  @Input()
+  public label?: string;
+
+  /** Font size for the label set by the `label` input.
+   *
+   * If not provided, then a value from the global Clarity styles is used.
+   * */
+  @Input()
+  public labelFontSizePx?: number;
+
+  /**
+   * Size (width & height) in pixels.
+   *
+   * NOTE: The inputs `widthPx` and `heightPx` have higher priority and override the according dimension.
+   * */
+  @Input()
+  public sizePx: number = BUTTON_DEFAULTS.buttonSizePx;
+
+  /**
+   * A string to be copied to the clipboard when the button is pressed.
+   *
+   * If not provided (or provided either as `null` or `undefined`), the button will be disabled.
+   */
+  @Input()
+  public textToCopy?: string | null;
+
+  /** A string to be shown as a tooltip on hovering the button */
+  @Input()
+  public title: string = BUTTON_DEFAULTS.title;
+
+  /**
+   * Width in pixels for the button. Includes the border size if `[withBorder]="true"`.
+   *
+   * If not provided, then a value from the `sizePx` input is used instead.
+   * */
+  @Input()
+  public widthPx?: number;
+
+  /** Whether to use animation inside the button */
+  @Input()
+  public withAnimation = true;
+
+  /** Whether to show a border around the button */
+  @Input()
+  public withBorder = false;
+
+  /** Emits a string successfully copied to the clipboard by pressing the button */
+  @Output()
+  public copied = new EventEmitter<string>();
+
+  /** Emits an unknown value on failed copying, caught from `navigator.clipboard.writeText()` */
+  @Output()
+  public failed = new EventEmitter<unknown>();
+
+  protected btnLoadingState: ClrLoadingState = ClrLoadingState.DEFAULT;
+
+  protected readonly clrLoadingState = ClrLoadingState;
+
+  private changeDetectionRef = inject(ChangeDetectorRef);
+
+  constructor() {
+    ClarityIcons.addIcons(copyIcon);
   }
 
-  @Input() disabled = false;
-  @Input() noVerticalMargins = true;
-  @Input() small = true;
-  @Input() textToCopy?: string | undefined;
-  @Input() title?: string;
-  @Input() withBorder = false;
-
-  @Output() copied = new EventEmitter<string>();
-  @Output() failed = new EventEmitter<unknown>();
-
-  btnLoadingState: ClrLoadingState = ClrLoadingState.DEFAULT;
-
-  readonly clrLoadingState = ClrLoadingState;
-
-  constructor(private changeDetectionRef: ChangeDetectorRef) {
+  protected get spinnerStylePatch(): Record<string, unknown> {
+    return {
+      'height.px': this.iconSizePx,
+      'width.px': this.iconSizePx,
+      'min-height.px': this.iconSizePx,
+      'min-width.px': this.iconSizePx,
+      'vertical-align': 'middle',
+    };
   }
 
-  copy(): void {
+  protected copy(): void {
     if (
       !navigator?.clipboard?.writeText ||
       !this.textToCopy ||
@@ -71,7 +149,7 @@ export class ButtonCopyToClipboardComponent {
       });
   }
 
-  resetToDefaultState(): void {
+  protected resetToDefaultState(): void {
     this.btnLoadingState = ClrLoadingState.DEFAULT;
   }
 }
