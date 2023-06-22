23a7fbdd8b4ce24c745e1bb82a4c66de038340d5
dmytro-batazov-ew <117995904+dmytro-batazov-ew@users.noreply.github.com>
2023-06-22 11:40:20 +0300

    Add docs and stories for router-link-wrapper component

---
+++ .storybook/stories/router-link-wrapper/router-link-wrapper.stories.ts
@@ -0,0 +1,119 @@
+import { provideRouter, RouterLink } from '@angular/router';
+import {
+  applicationConfig,
+  type Meta,
+  moduleMetadata,
+  type StoryObj,
+} from '@storybook/angular';
+
+import { RouterLinkWrapperComponent } from '../../../projects/extra-clarity/router-link-wrapper';
+import { hideAllControlRows } from '../../helpers';
+
+type Story = StoryObj<RouterLinkWrapperComponent>;
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+const MetaStory = {
+  title: 'Components/Router Link Wrapper',
+  component: RouterLinkWrapperComponent,
+  decorators: [
+    applicationConfig({
+      providers: [
+        provideRouter([
+          { path: 'home', canActivate: [() => false], component: RouterLinkWrapperComponent },
+          { path: '**', redirectTo: '' },
+        ]),
+      ],
+    }),
+  ],
+  parameters: { controls: { sort: 'requiredFirst' } },
+  argTypes: {
+    // INPUTS
+    enabled: {
+      type: {
+        name: 'boolean',
+        required: true,
+      },
+    },
+    fwdRouterLink: {
+      type: {
+        name: 'other',
+        value: '',
+        required: true,
+      },
+    },
+    queryParams: {
+    },
+  },
+  args: {
+    enabled: true,
+    fwdRouterLink: '/',
+    queryParams: undefined,
+  },
+} as Meta<RouterLinkWrapperComponent>;
+
+export default MetaStory;
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export const WrapperItselfStory: Story = {
+  name: 'Story: Wrapper itself',
+  decorators: [
+    moduleMetadata({
+      imports: [
+        RouterLinkWrapperComponent,
+      ],
+    }),
+  ],
+  render: (args) => ({
+    props: args,
+    template: `
+      <ec-router-link
+        [enabled]="enabled"
+        [fwdRouterLink]="fwdRouterLink"
+        [queryParams]="queryParams"
+      >
+        Link label
+      </ec-router-link>
+    `,
+  }),
+  args: {
+  },
+};
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export const DocsExampleStory: Story = {
+  name: 'Story: Example for docs',
+  decorators: [
+    moduleMetadata({
+      imports: [
+        RouterLink,
+        RouterLinkWrapperComponent,
+      ],
+    }),
+  ],
+  render: (args) => ({
+    props: args,
+    template: `
+      <ec-router-link [enabled]="true" [fwdRouterLink]="['/about']">
+        Enabled link within the wrapper
+      </ec-router-link>
+      <br />
+      <ec-router-link [enabled]="false" [fwdRouterLink]="['/about']">
+        Disabled link within the wrapper
+      </ec-router-link>
+      <br />
+      <a [routerLink]="['/about']">
+        Enabled link without the wrapper
+      </a>
+      <br />
+      <a [routerLink]="null">
+        Disabled link without the wrapper
+      </a>
+    `,
+  }),
+  // hide all controls since the purpose of this story is a hard-coded demo in the docs page
+  argTypes: {
+    ...hideAllControlRows(MetaStory.argTypes),
+  },
+  args: {
+  },
+};
