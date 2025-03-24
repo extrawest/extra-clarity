4a52a148bdc1f2be9902d6700eb23f6c6d24e2cd
dmytro-batazov-ew <117995904+dmytro-batazov-ew@users.noreply.github.com>
2025-03-24 22:48:26 +0200

    chore(eslint): migrate to ESLint v9 and flat configs

---
+++ .storybook/eslint.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+import tseslint from 'typescript-eslint';
+import rootConfig from '../eslint.config.mjs';
+
+export default tseslint.config(
+  ...rootConfig,
+  {
+    files: ['**/*.ts'],
+    rules: {},
+  },
+  {
+    files: ['**/*.mdx'],
+    rules: {},
+  },
+);
