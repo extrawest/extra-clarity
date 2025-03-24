efb571f91b4f548b2ea46852e78361eed8e7fee3
dmytro-batazov-ew <117995904+dmytro-batazov-ew@users.noreply.github.com>
2025-03-25 00:46:18 +0200

    build(extra-clarity): import local source files instead of built dist

--- projects/extra-clarity/eslint.config.mjs
+++ projects/extra-clarity/eslint.config.mjs
@@ -5,6 +5,16 @@ import rootConfig from '../../eslint.config.mjs';
 export default tseslint.config(
   ...rootConfig,
   {
+    settings: {
+      // This is needed for 'eslint-plugin-import' working properly with imports in .ts files
+      'import/resolver': {
+        typescript: {
+          project: './tsconfig.lib.json',
+        },
+      },
+    },
+  },
+  {
     files: ['**/*.ts'],
     rules: {},
   },
