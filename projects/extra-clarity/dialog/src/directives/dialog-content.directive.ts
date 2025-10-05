e3f014438e4a767de98764b03b262f30e9e0fdce
dmytro-batazov-ew <117995904+dmytro-batazov-ew@users.noreply.github.com>
2025-10-05 14:15:52 +0300

    fix(dialog): add missing "modal-body" class for injected dialog content

--- projects/extra-clarity/dialog/src/directives/dialog-content.directive.ts
+++ projects/extra-clarity/dialog/src/directives/dialog-content.directive.ts
@@ -4,6 +4,9 @@ import { Directive } from '@angular/core';
   // eslint-disable-next-line @angular-eslint/directive-selector
   selector: '[dialog-content]',
   exportAs: 'dialogContent',
+  host: {
+    class: 'modal-body',
+  },
   standalone: false,
 })
 export class DialogContentDirective {}
